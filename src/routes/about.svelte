cf0525af60ac77ff9ad8ffc76f7ea3eea06af145
sohan2344 <gowdasohan09@gmail.com>
2023-04-15 16:06:05 +0530

    Initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import SplitioIcon from '$lib/SplitioIcon.svelte';
+	import Button, { Label, Icon } from '@smui/button';
+	import Paper, { Title, Content } from '@smui/paper';
+</script>
+
+<svelte:head>
+	<title>splitio | about</title>
+</svelte:head>
+
+<div class="homepage-container">
+	<div>
+		<SplitioIcon />
+	</div>
+	<div class="group-text-container">
+	</div>
+</div>
+
+<style>
+	.homepage-container {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.group-text-container {
+		margin-top: 1.5rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	* :global(.info-btn) {
+		position: absolute;
+		top: 1rem;
+		right: 1rem;
+	}
+
+	* :global(.paper-demo) {
+		margin: 0 1rem;
+		max-width: 600px;
+	}
+</style>
